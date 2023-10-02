f052829e56c41692d1677c5b4cfb16792feedb5a
AppleIsCute <56248033+AppleIsCute@users.noreply.github.com>
2023-10-02 17:23:27 +0800

    Update keyboard_manage.js

--- keyboard_manage.js
+++ keyboard_manage.js
@@ -1,8 +1,10 @@
+
 let updown01 = ['q','2','w','3','e','4','r','t', '6', 'y', '7', 'u', 'i','9', 'o', '0', 'p', '-',"[","]"]; // start from f3
 let updown02 = ['z', 's', 'x', 'd', 'c', 'v', 'g', 'b', 'h', 'n','j', 'm',  ",", 'l', ".", ";", "/"]; /// start from c3
 var gaswrite = [];
 var writer;
 var recorder = [];
+
 //var srollttest=20;
 
 
@@ -29,7 +31,7 @@ var recorder = [];
             //C7: "C7v14.mp3",
 
         },
-        release: 1,
+       release: 0.5,
         volume: 1.0,
 
 //baseUrl: "https://unpkg.com/@audio-samples/piano-mp3-velocity14@1.0.5/audio/",
@@ -56,7 +58,10 @@ window.addEventListener('keydown', function (e) {
                 var targetDiv = document.getElementById("key" + (j +1+29));
                 targetDiv.style.background = 'linear-gradient(#E74949 96%, #CA1717 4%)';
 
-            piano.triggerAttackRelease(note_sequence[j+29], tone_length);
+           // piano.triggerAttackRelease(note_sequence[j+29], tone_length);
+
+            piano.triggerAttack(note_sequence[j + 29]);
+
 
             NowChord.push(53 + j);
             make_sheet(note_sequence[j + 29]);
@@ -72,13 +77,13 @@ window.addEventListener('keydown', function (e) {
                 test[i] = test[i].toString();
 
                 gaswrite[i] = Tonal.AbcNotation.scientificToAbcNotation(test[i])+longer;// to abcjs form
-
-
+                
+               //if (test[i] == 'Z') { gaswrite[i] = "z" + longer;}
 
                 if (test.length > 1) {
                     writer = "[" + gaswrite.join("") + "]"
                 }
-                else { writer = "" + gaswrite + ""; }
+                else { writer = " " + gaswrite + " "; }
             }
 
 
@@ -103,8 +108,10 @@ window.addEventListener('keydown', function (e) {
             var targetDiv = document.getElementById("key" + (j + 1 + 24));
             targetDiv.style.background = 'linear-gradient(#E74949 96%, #CA1717 4%)';
 
-            piano.triggerAttackRelease(note_sequence[j + 24], tone_length);
+            //piano.triggerAttackRelease(note_sequence[j + 24], tone_length);
 
+
+            piano.triggerAttack(note_sequence[j + 24]);
             NowChord.push(48 + j);
             make_sheet(note_sequence[j + 24]);
 
@@ -115,17 +122,17 @@ window.addEventListener('keydown', function (e) {
                 
                 test[i] = test[i].toString();
 
-                if (longer == 1) { longer = "1 "; } else { };
+               //if (test[i] == 'Z') { gaswrite[i] = "z" + longer; }
 
                 gaswrite[i] = Tonal.AbcNotation.scientificToAbcNotation(test[i]) + longer;// to abcjs form
                 
-
+                
                
 
                 if (test.length > 1) {
                     writer = "[" + gaswrite.join("") + "]"
                 }
-                else { writer = ""+gaswrite+""; }     
+                else { writer = " "+gaswrite+" "; }     
             }
 
             
@@ -142,16 +149,21 @@ window.addEventListener('keydown', function (e) {
 
         }
 
-      
-
+       
         
     }
+
+   
 
     
     //NowChord.sort();
     Chord_detect();
     
-  
+    
+
+
+
+
     
 
 }, false);
@@ -166,6 +178,7 @@ window.addEventListener('keydown', function (e) {
 
 
 document.addEventListener('keyup', function (event) {
+    if (event.repeat) { return; }
     var keyID = event.key;
 
 
@@ -175,6 +188,9 @@ document.addEventListener('keyup', function (event) {
 
             var targetDiv = document.getElementById("key" + (j + 1 + 29));
             var i = j + 29;
+
+
+            piano.triggerRelease(note_sequence[j + 29]);
 
             if (i % 12 === 0 || i % 12 === 2 || i % 12 === 4 || i % 12 === 5 || i % 12 === 7 || i % 12 === 9 || i % 12 === 11) { targetDiv.style.background = 'linear-gradient(#fff 96%, #eee 4%)'; }
             else { targetDiv.style.background = 'linear-gradient(#333 96%, #525050 4%)'; }
@@ -196,12 +212,10 @@ document.addEventListener('keyup', function (event) {
 
                 //srollttest += 10;
 
-                staff_add += 30;
+                if (staff_add >= 800) { staff_add += 30; }
+                else { staff_add += 10; }
 
-                //const now = document.getElementById("now");
-                //now.style.left = (staff_add - 830) + "px";
-                //scroll[0].scrollLeft = srollttest ;
-                //console.log(srollttest);
+               
 
 
 
@@ -221,6 +235,8 @@ document.addEventListener('keyup', function (event) {
         if (event.key === updown02[j]) {
 
             var targetDiv = document.getElementById("key" + (j + 1 + 24));
+            piano.triggerRelease(note_sequence[j + 24]);
+
             var i = j + 24;
             if (i % 12 === 0 || i % 12 === 2 || i % 12 === 4 || i % 12 === 5 || i % 12 === 7 || i % 12 === 9 || i % 12 === 11) { targetDiv.style.background = 'linear-gradient(#fff 96%, #eee 4%)'; }
             else { targetDiv.style.background = 'linear-gradient(#333 96%, #525050 4%)'; }
@@ -243,16 +259,15 @@ document.addEventListener('keyup', function (event) {
                 scroll[0].scrollLeft = scroll[0].scrollWidth;
                
 
+                if (staff_add >= 800) { staff_add += 30;}
+                else { staff_add += 10; }
               
-                staff_add += 30;
-              
-                //const now = document.getElementById("now");
-                //now.style.left = (staff_add -830) + "px";
+                
 
 
             }
 
-           // console.log(clef_type);
+           
 
 
             ///important lines//
@@ -260,7 +275,7 @@ document.addEventListener('keyup', function (event) {
             test.length = 0;
             gaswrite.length = 0;
             writer = "";
-
+            console.log(recorder);
             ///important lines//
         }
 
@@ -272,10 +287,39 @@ document.addEventListener('keyup', function (event) {
             
         _cal.length=0;
     }
+
+    if (event.key === "Backspace") {
+        
+
+        recorder.pop();
+        console.log(recorder);
+        abcString = "X: 1 T: \nV: 1 " + "clef = " + clef_type + " \n K: \n" + recorder.join('') + writer;
+
+        load();
+
+    }
+
+    if (event.code === "Backslash") {
+
+
+      recorder.push(" " +'|' + " ");
+        abcString = "X: 1 T: \nV: 1 " + "clef = " + clef_type + " \n K: \n" + recorder.join('') + writer;
+
+        load();
+
+    }
+
+    if (event.key === " ") {
+
+
+        recorder.push(" " + 'z' + longer+" ");
+        abcString = "X: 1 T: \nV: 1 " + "clef = " + clef_type + " \n K: \n" + recorder.join('') + writer;
+
+        load();
+
+    }
   
-    
+    console.log(abcString);
 
 },false);
-
-
 
